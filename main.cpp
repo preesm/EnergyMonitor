d9e737c6350b8764fd804dd0e2a408efcba31560
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-02-28 16:58:33 +0100

    Update main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
-#include "displaysysinfo.h"
-#include <QApplication>
+#include "getnode.h"
+#include <unistd.h>
+#include <iostream>
+using namespace std;
+
+class GetNode;
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    DisplaySysInfo w;
-    w.show();
-
-    return a.exec();
+  GetNode *getNode;
+  getNode = new GetNode();
+  getNode->OpenINA231();
+  
+  int updatePeriod = getNode->getUpdatePeriodMicroSec();
+  while (1) {
+    usleep(updatePeriod);
+    // probe values
+    getNode->GetINA231();
+    
+    cout << "probe" << "\n";
+    cout << getNode->armuW << " W" << "\n";
+    
+    //
+  }
+  return 0;
 }

7eb6da2e1d1d7ce579d44893f6f79cf0ac7b72f0
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-03-01 15:52:39 +0100

    update main and gitignore

--- main.cpp
+++ main.cpp
@@ -1,17 +1,44 @@
 #include "getnode.h"
 #include <unistd.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
 using namespace std;
 
 class GetNode;
 
+int main(int argc, char ** argv) {
 
+  string resultDirPath = ".";
+  if (argc == 0) {
+  } else if (argc == 1) {
+    cout << "using current dir for outputting results\n";
+  } else if (argc == 2) {
+    cout << "using '" << argv[1] << "' for outputting results\n";
+    std::ostringstream oss;
+    oss << argv[1];
+    resultDirPath = oss.str();
+  } else  {
+    cerr << "too many args.\n";
+    return 1;
+  }
 
-int main() {
+  ofstream a7wFile;
+  ofstream a15wFile;
+  ofstream gpuwFile;
+  ofstream memwFile;
+
+  a7wFile.open ("a7w.csv");
+  a15wFile.open ("a15w.csv");
+  gpuwFile.open ("gpuw.csv");
+  memwFile.open ("memw.csv");
+
   GetNode *getNode;
   getNode = new GetNode();
   getNode->OpenINA231();
-  
+
   int updatePeriod = 100000; //us
   while (1) {
     // probe values every updatePeriod us
@@ -19,8 +46,10 @@ int main() {
     getNode->GetINA231();
     
     // outptu values
-    // todo ...
-    
+    float A15W = getNode->armuW;
+    float GPUW = getNode->g3duW;
+    float A7W = getNode->kfcuW;
+    float MEMW = getNode->memuW;
   }
   return 0;
 }
